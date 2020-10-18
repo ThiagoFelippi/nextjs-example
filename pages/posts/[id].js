16e73aed79706aef4ddb82989f7d8d0fa3edfd0c
thiago <thiagocrespo241103@gmail.com>
2020-10-18 16:40:29 -0300

    Dinamic posts done

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,38 +1,49 @@
 import React from 'react';
 import style from "../../styles/Post.module.scss"
+import { join } from "path"
+import { readdirSync, readFileSync } from 'fs';
 
-function Post() {
+function Post({title, content, author}) {
+
+  console.log(author)
 
   return (
     <div className={style.container}>
       <h1 className={style.title}>
-        ola
+        {title}
       </h1>
+      <p>
+        {content}
+      </p>
+      <span> {author} </span>
     </div>
   )
 }
 
 export const getStaticPaths = () => {
+  const posts = readdirSync("posts").map(post => post.split(".")[0])
+ 
+  const paths = posts.map(post => ({
+    params: {
+      id: post
+    }
+  }))
 
   return {
-    paths: [
-      { 
-        params: {
-          id: "ola"
-        }
-      }
-    ],
+    paths, 
     fallback: false,
   }
 }
 
 export const getStaticProps = async (context) => {
   const {id} = context.params
+  const postPath = join("posts", `${id}.json`)
+  const fileJSON = readFileSync(postPath, 'utf8')
+  const {title, content, author} = JSON.parse(fileJSON)
 
   return {
-    props: {
-      id
-    }
+    props:
+      {title, content, author}
   }
 }
 
